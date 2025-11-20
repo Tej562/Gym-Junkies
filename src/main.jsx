a8f66a1d7019e3a4be5b5f83b3d1fbfc07cb2bfa
Tej562 <u2326380@uel.ac.uk>
2025-11-21 00:18:20 +0530

    fix(router): wrap App with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./context/theme";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
